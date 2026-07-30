df54001d8d8f1b449ce630431d2896acfa011c6a
Bulbul verma <bulbulverma6266@gmail.com>
2026-07-30 18:04:32 +0530

    fix: add socket auth token fallback

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,5 +1,10 @@
 const env = import.meta.env
 
 import {io} from 'socket.io-client'
-const socket = io(env.VITE_SERVER, {withCredentials: true})
+const socket = io(env.VITE_SERVER, {
+    withCredentials: true,
+    auth: {
+        token: typeof window !== 'undefined' ? localStorage.getItem("accessToken") || "" : ""
+    }
+})
 export default socket
